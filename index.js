abae811e896c18e9a3176869f27a0f50a9da8c5d
mahi8026 <mahimrahman07@gmail.com>
2025-11-12 21:59:04 +0600

    add course

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("learnloopDB");
+    const coursesCollection = db.collection("courses");
+    const enrollmentsCollection = db.collection("enrollments");
+    console.log(" Connected to MongoDB");
+
+    const router = express.Router();
+
+    router.post("/courses", async (req, res) => {
+      const result = await coursesCollection.insertOne(req.body);
+      res.status(201).json({ success: true, data: result });
+    });
+
+    
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+  } catch (error) {
+    console.error(" Server initialization error:", error);
+  }
+}
+
+run().catch(console.dir);
